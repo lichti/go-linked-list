faa78eae720c4ac7e76f8526af1145093f2ecf38
Lichti <gustavo.lichti@gmail.com>
2017-09-06 19:05:28 -0300

    add same docs

--- README.md
+++ README.md
@@ -1,26 +1,163 @@
-# go-linked-list
-Go lib to create and manipule linked lists
+# linkedlist
+--
+    import "github.com/lichti/go-linked-list"
 
-# Simple example
-```
-package main
 
-import (
-	"github.com/lichti/go-linked-list"
-)
+## Usage
 
-func main() {
-	var input linkedlist.List
-	var output linkedlist.List
-	input.Load("entrada.txt")
-	output.Copy(input)
-	output.Inversor()
-	output.Save("saida.txt")
+#### type Item
+
+```go
+type Item struct {
+	Value interface{}
+	Next  *Item
 }
 ```
 
-# License
-GPLv3
+Item stores the value and a pointer to the next Item, if the pointer is null,
+this Item is the last of the linked list. Example:
 
-# Author
- - [Gustavo Lichti](gustavo.lichti@gmail.com)
+    var myList linkedlist.List
+    myList.Push("one")
+    myList.Push("Two")
+    myList.Push("Three")
+    fmt.Println(myList.First.Value)
+    fmt.Println(myList.First.Next.Value)
+    fmt.Println(myList.First.Next.Next.Value)
+    fmt.Println(myList.First.Next.Next.Next)
+
+#### type List
+
+```go
+type List struct {
+	First *Item
+}
+```
+
+List is the main object this library, it's she that allows us to create and
+manipulate the linked list. Example:
+
+    var myList linkedlist.List
+    myList.Push("One")
+    myList.Push("Two")
+    myList.Push("Three")
+    fmt.Println(myList.Len())
+    myList.Remove("Two")
+    fmt.Println(myList.Len())
+    myList.Print()
+
+#### func (*List) Copy
+
+```go
+func (l *List) Copy(origin List)
+```
+Copy the itens from one linked list to tail of the other linked list. Example:
+
+    var myList linkedlist.List
+    var myOtherList linkedlist.List
+    myList.Push("One")
+    myList.Push("Two")
+    myOtherList.Push("Three")
+    myOtherList.Push("Four")
+    myOtherList.Copy(myList)
+    myOtherList.Print()
+
+#### func (*List) Find
+
+```go
+func (l *List) Find(value interface{}) bool
+```
+Find an item in the linked list and return true if it exists or false, if it
+does not exist. Example:
+
+    var myList linkedlist.List
+    myList.Push("One")
+    myList.Push("Two")
+    fmt.Println(myList.Find("Two"))
+    fmt.Println(myList.Find("Three"))
+
+#### func (*List) Inversor
+
+```go
+func (l *List) Inversor()
+```
+Invert the linked list. Example:
+
+    var myList linkedlist.List
+    myList.Push("One")
+    myList.Push("Two")
+    myList.Inversor()
+    myList.Print()
+
+#### func (*List) Len
+
+```go
+func (l *List) Len() int
+```
+Len returns the size of the linked list. Example:
+
+    var myList linkedlist.List
+    myList.Push("One")
+    myList.Push("Two")
+    myList.Push("Three")
+    fmt.Println(myList.Len())
+
+#### func (*List) Load
+
+```go
+func (l *List) Load(path string) error
+```
+Load list from file and push into tail of the linked list. Example:
+
+    var myList linkedlist.List
+    myList.Push("One")
+    myList.Load("input.txt")
+
+#### func (*List) Print
+
+```go
+func (l *List) Print()
+```
+Print the linked list to stdOut. Example:
+
+    var myList linkedlist.List
+    myList.Push("One")
+    myList.Push("Two")
+    myList.Print()
+
+#### func (*List) Push
+
+```go
+func (l *List) Push(value interface{})
+```
+Push a new item into tail of the linked list. Example:
+
+    var myList linkedlist.List
+    myList.Push("One")
+    myList.Push("Two")
+
+#### func (*List) Remove
+
+```go
+func (l *List) Remove(value interface{}) bool
+```
+Remove a item of the linked list and return a bool value. Example:
+
+    var myList linkedlist.List
+    myList.Push("One")
+    myList.Push("Two")
+    fmt.Println(myList.Remove("Two"))
+    fmt.Println(myList.Remove("Three"))
+    fmt.Println(myList.Remove("Two"))
+
+#### func (*List) Save
+
+```go
+func (l *List) Save(path string) error
+```
+Save the linked list to file. Example:
+
+    var myList linkedlist.List
+    myList.Push("One")
+    myList.Push("Two")
+    myList.Save("/tmp/myList.txt")
--- main.go
+++ main.go
@@ -1,3 +1,5 @@
+// Package linkedlist must help you  to create a linked list and provide some methods to easy manipule this list.
+
 package linkedlist
 
 import (
@@ -6,23 +8,49 @@ import (
 	"os"
 )
 
-// Item .
+// List is the main object this library, it's she that allows us to create and manipulate the linked list. Example:
+//
+//     var myList linkedlist.List
+//     myList.Push("One")
+//     myList.Push("Two")
+//     myList.Push("Three")
+//     fmt.Println(myList.Len())
+//     myList.Remove("Two")
+//     fmt.Println(myList.Len())
+//     myList.Print()
+//
+type List struct {
+	First *Item
+}
+
+// Item stores the value and a pointer to the next Item, if the pointer is null, this Item is the last of the linked list. Example:
+//
+//     var myList linkedlist.List
+//     myList.Push("one")
+//     myList.Push("Two")
+//     myList.Push("Three")
+//     fmt.Println(myList.First.Value)
+//     fmt.Println(myList.First.Next.Value)
+//     fmt.Println(myList.First.Next.Next.Value)
+//     fmt.Println(myList.First.Next.Next.Next)
+//
 type Item struct {
 	Value interface{}
 	Next  *Item
 }
 
-// List .
-type List struct {
-	First *Item
-}
-
-// Len return size of list
+// Len returns the size of the linked list. Example:
+//
+//     var myList linkedlist.List
+//     myList.Push("One")
+//     myList.Push("Two")
+//     myList.Push("Three")
+//     fmt.Println(myList.Len())
+//
 func (l *List) Len() int {
 	p := l.First
 	var i int
 	i = 0
-
 	for p.Next != nil {
 		p = p.Next
 		i++
@@ -33,7 +61,12 @@ func (l *List) Len() int {
 	return i
 }
 
-// Push a new item into list
+// Push a new item into tail of the linked list. Example:
+//
+//     var myList linkedlist.List
+//     myList.Push("One")
+//     myList.Push("Two")
+//
 func (l *List) Push(value interface{}) {
 	n := &Item{value, nil}
 	if l.First == nil {
@@ -47,7 +80,14 @@ func (l *List) Push(value interface{}) {
 	}
 }
 
-// Find a Item into List
+// Find an item in the linked list and return true if it exists or false, if it does not exist. Example:
+//
+//     var myList linkedlist.List
+//     myList.Push("One")
+//     myList.Push("Two")
+//     fmt.Println(myList.Find("Two"))
+//     fmt.Println(myList.Find("Three"))
+//
 func (l *List) Find(value interface{}) bool {
 	var p *Item
 	if l.First == nil {
@@ -63,7 +103,15 @@ func (l *List) Find(value interface{}) bool {
 	return false
 }
 
-// Remove item of list and return a bool value
+// Remove a item of the linked list and return a bool value. Example:
+//
+//     var myList linkedlist.List
+//     myList.Push("One")
+//     myList.Push("Two")
+//     fmt.Println(myList.Remove("Two"))
+//     fmt.Println(myList.Remove("Three"))
+//     fmt.Println(myList.Remove("Two"))
+//
 func (l *List) Remove(value interface{}) bool {
 	var p *Item
 	if l.Find(value) == false {
@@ -87,7 +135,13 @@ func (l *List) Remove(value interface{}) bool {
 	return true
 }
 
-// Print List to stdOut
+// Print the linked list to stdOut. Example:
+//
+//     var myList linkedlist.List
+//     myList.Push("One")
+//     myList.Push("Two")
+//     myList.Print()
+//
 func (l *List) Print() {
 	p := l.First
 	for p != nil {
@@ -96,7 +150,12 @@ func (l *List) Print() {
 	}
 }
 
-// Load list from file
+// Load list from file and push into tail of the linked list. Example:
+//
+//     var myList linkedlist.List
+//     myList.Push("One")
+//     myList.Load("input.txt")
+//
 func (l *List) Load(path string) error {
 	file, err := os.Open(path)
 	if err != nil {
@@ -110,7 +169,13 @@ func (l *List) Load(path string) error {
 	return scanner.Err()
 }
 
-// Save linked list to file
+// Save the linked list to file. Example:
+//
+//     var myList linkedlist.List
+//     myList.Push("One")
+//     myList.Push("Two")
+//     myList.Save("/tmp/myList.txt")
+//
 func (l *List) Save(path string) error {
 	file, err := os.Create(path)
 	if err != nil {
@@ -126,7 +191,14 @@ func (l *List) Save(path string) error {
 	return w.Flush()
 }
 
-// Invert linked list
+// Invert the linked list. Example:
+//
+//     var myList linkedlist.List
+//     myList.Push("One")
+//     myList.Push("Two")
+//     myList.Inversor()
+//     myList.Print()
+//
 func (l *List) Inversor() {
 	var prev *Item
 	curr := l.First
@@ -139,7 +211,17 @@ func (l *List) Inversor() {
 	l.First = prev
 }
 
-// Copy itens from other linked list
+// Copy the itens from one linked list to tail of the other linked list. Example:
+//
+//     var myList linkedlist.List
+//     var myOtherList linkedlist.List
+//     myList.Push("One")
+//     myList.Push("Two")
+//     myOtherList.Push("Three")
+//     myOtherList.Push("Four")
+//     myOtherList.Copy(myList)
+//     myOtherList.Print()
+//
 func (l *List) Copy(origin List) {
 	p := origin.First
 	for p.Next != nil {
